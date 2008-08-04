d725c9f62a692c7df2ba08bd51a8d0602ddfbc5f
Bruce Williams <bruce@codefluency.com>
2008-08-04 12:51:28 -0500

    Support for finding current Rails controller/action context

--- dash/rails.rb
+++ dash/rails.rb
@@ -1,4 +1,9 @@
-Fiveruns::Dash.register_recipe :rails do |metrics|
+Fiveruns::Dash.register_recipe :rails do |recipe|
+  
+  recipe.included do
+    require File.dirname(__FILE__) << "/../lib/fiveruns/dash/rails"
+    ActionController::Base.send(:include, Fiveruns::Dash::Rails::Context)
+  end
   
   # TODO
   
--- lib/fiveruns/dash/rails.rb
+++ lib/fiveruns/dash/rails.rb
@@ -2,6 +2,53 @@ module Fiveruns
   module Dash
     
     module Rails
+      
+      module Context
+      
+        def self.included(base)
+          base.send(:include, InstanceMethods)
+          base.alias_method_chain :perform_action, :fiveruns_dash_context
+        end
+      
+        def self.controller_context
+          if context
+            context[0, 2]
+          end
+        end
+      
+        def self.action_context
+          if context
+            context[2, 2]
+          end
+        end
+        
+        def self.set(value)
+          @context = value
+          if block_given?
+            yield
+            reset
+          end
+        end
+        
+        def self.reset
+          @context = nil
+        end
+      
+        class << self
+          attr_reader :context
+        end
+      
+        module InstanceMethods
+          def perform_action_with_fiveruns_dash_context(*args, &block)
+            action_name = (request.parameters['action'] || 'index').to_s
+            Fiveruns::Dash::Rails::Context.set [:controller, controller_name, :action, action_name] do
+              perform_action_without_fiveruns_dash_context(*args, &block)
+            end
+          end
+        end
+        
+      end
+      
     end
     
   end
