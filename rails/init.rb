ed86cfb6f34a8e147db1476e0b9ab631f55ec15e
Bruce Williams <bruce@codefluency.com>
2008-08-08 13:19:11 -0500

    Add important TODO

--- rails/init.rb
+++ rails/init.rb
@@ -9,6 +9,7 @@ Fiveruns::Dash.configure do |config|
   config.add_recipe :rails, :url => 'http://dash.fiveruns.com'
 end
 
+# TODO: Only start-up when running the server
 require 'dispatcher'
 Dispatcher.to_prepare :check_configuration do
   Fiveruns::Dash::Rails.start
