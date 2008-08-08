e001ed4536517395e04a1f32603ddbd35bec0a6d
Bruce Williams <bruce@codefluency.com>
2008-08-07 20:07:48 -0500

    Make setting app tokens for different environments easier

--- lib/fiveruns/dash/rails.rb
+++ lib/fiveruns/dash/rails.rb
@@ -23,17 +23,27 @@ module Fiveruns
         end
       end
       
+      def self.configure(tokens = {}, &block)
+        tokens.each do |environment, token|
+          if environment.to_s == ::Rails.env
+            Fiveruns::Dash.configure({:app => token}, &block)
+            break
+          end
+        end
+      end
+      
       def self.log_error
         # TODO: Add URL for help
         message =<<-EOM
-    FiveRuns Dash [Rails] (v#{Version::STRING}) Application token missing
-      ===
-      In config/initializers/dash.rb or at the bottom of config/environment.rb, please add:
+FiveRuns Dash [Rails] (v#{Version::STRING}) Application token missing
+  ===
+  In config/initializers/dash.rb or at the bottom of config/environment.rb, please add:
 
-        Fiveruns::Dash.configure :app => 'YOUR-APP-TOKEN-HERE'
+    Fiveruns::Dash::Rails.configure :#{::Rails.env} => 'YOUR-#{::Rails.env.upcase}-ENV-APP-TOKEN-HERE'
 
-      See http://todo/path/to/help
-      ===
+  You can also set app tokens for other environments (eg, staging), at the same time.
+  See http://todo/path/to/help for more information
+  ===
         EOM
         RAILS_DEFAULT_LOGGER.warn(message.strip)
       end
