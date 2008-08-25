42c2d240f73ecc0662103c2fb31318fa90fd5a06
Bruce Williams <bruce@codefluency.com>
2008-08-25 15:26:34 -0500

    Cleanup

--- dash/rails.rb
+++ dash/rails.rb
@@ -1,22 +1,3 @@
-# * Average Action Response Time: per 1 min interval, most important metric to me?
-# ** Captured at 'response_time' metric by 'actionpack' recipe
-# * Action Requests?: per 1min interval, has to accompany 1 above?
-# ** Captured as 'requests' metric by 'actionpack' recipe
-# * ActiveRecord Activity Indicator: ?percent per 1 min interval, aggregate metric, tbd?
-# ** Captured as 'activity' metric by 'activerecord' recipe
-# * Includes slowest, most frequently called actions/models information
-# ** This feature is provided by the 'response_time' (actionpack) and 'activity' (activerecord)
-#    metrics and the metric store structure; a function of how the data is sliced
-# * Active Rails processes: unique PIDs reporting per interval, smoothed out for restarts?
-# ** These are captured as the number of 'Process' records
-# * Aggregated Rails Process CPU Usage: ?percent per 1 min interval?
-# ** Captured as 'cpu' metric by 'ruby' recipe
-# * Aggregated Rails Memory Usage?: percent per 1 min interval?
-# ** Captured as 'pmem' remtric by 'ruby' recipe
-# ** Also capturing as bytes in 'rss' metric by 'ruby' recipe
-# * Mongrel Queue Size?: If available?, alternative metric for thin?
-
-
 Fiveruns::Dash.register_recipe :actionpack, :url => 'http://dash.fiveruns.com' do |recipe|
   recipe.time :response_time, :method => 'ActionController::Base#perform_action'
   recipe.counter :requests, 'Requests', :incremented_by => 'ActionController::Base#perform_action'
