6f034f7d0d2393683e8415c11741956927c7ad08
Bruce Williams <bruce@codefluency.com>
2008-09-30 12:39:37 -0500

    Oops, forgot a file

---
+++ lib/fiveruns/dash/rails/startup.rb
@@ -0,0 +1,18 @@
+START_FIVERUNS_DASH_RAILS = if ENV['START_FIVERUNS_DASH'] || File.basename($0) != 'irb'
+  true
+else
+  module Fiveruns
+    module Dash
+      module Rails
+        
+        def self.configure(*args, &block)
+          RAILS_DEFAULT_LOGGER.info "[FiveRuns Dash] Skipping configuration (`#{$0}' not supported for collection)"
+        end
+        
+      end
+    end
+  end
+  false
+end
+  
+  
