3151cebec1060befe5f65241061ac67c74390bd3
Mike Perham <mperham@gmail.com>
2008-12-18 12:01:46 -0600

    Don't require file.  Class has moved into the AR recipe.

--- lib/fiveruns_dash_rails.rb
+++ lib/fiveruns_dash_rails.rb
@@ -5,6 +5,5 @@ if START_FIVERUNS_DASH_RAILS
   require 'fiveruns/dash'
   require 'fiveruns/dash/rails'
   require 'fiveruns/dash/template_context'
-  require 'fiveruns/dash/active_record_context'
   require File.dirname(__FILE__) << "/../rails/init"  
 end
