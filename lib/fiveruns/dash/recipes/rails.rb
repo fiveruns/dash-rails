4e72dff0ed2148604583d4ff3d13b88a51aa37b1
Mike Perham <mperham@gmail.com>
2009-02-17 15:19:58 -0600

    Update Passenger support to use 2.1's event callbacks.

--- lib/fiveruns/dash/recipes/rails.rb
+++ lib/fiveruns/dash/recipes/rails.rb
@@ -105,19 +105,23 @@ Fiveruns::Dash.register_recipe :rails, :url => 'http://dash.fiveruns.com' do |re
       end
     end
 
-    # Passenger forks the Rails processes, which has the side effect of
-    # killing our reporter thread.  We need to revive the thread.
-    class ActionController::Base
-      def perform_action_with_dash_startup(*args, &block)
-        Fiveruns::Dash.session.reporter.revive!
-        perform_action_without_dash_startup(*args, &block)
-      end
+    if defined?(Passenger)
+      # Passenger forks the Rails processes, which has the side effect of
+      # killing our reporter thread.  We need to revive the thread.
+      class ActionController::Base
+        def perform_action_with_dash_startup(*args, &block)
+          Fiveruns::Dash.session.reporter.revive!
+          perform_action_without_dash_startup(*args, &block)
+        end
 
-      alias_method_chain :perform_action, :dash_startup
+        alias_method_chain :perform_action, :dash_startup
+      end
+    elsif defined?(PhusionPassenger)
+      # later versions of Passenger have an event to handle this gracefully
+      PhusionPassenger.on_event(:starting_worker_process) do
+        Fiveruns::Dash.session.reporter.revive!
+      end
     end
   end
   
-  # recipe.absolute :queue_size do
-  #   Fiveruns::Dash::Rails.queue_size || 0
-  # end
 end
